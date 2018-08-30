3146af246827763dbf3e90d80d4bbf0cb990adfd
PawelPodkalicki <ppodkali@gmail.com>
2018-08-30 11:29:11 +0200

    Clear manifest permissions

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -2,31 +2,6 @@
 
 console.log('popup.js' + new Date());
 
-// let changeColor = document.getElementById('changeColor');
-// chrome.storage.sync.get('color', function (data) {
-//     changeColor.style.backgroundColor = data.color;
-//     changeColor.setAttribute('value', data.color);
-// });
-// changeColor.onclick = function (element) {
-//     let color = element.target.value;
-//     chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-//         chrome.tabs.executeScript(
-//             tabs[0].id,
-//             // {code: 'document.body.style.backgroundColor = "' + color + '";'}
-//             {
-//                 code: 'var scriptTag = document.createElement(\'script\');\n' +
-//                     '    scriptTag.textContent = \'let a = 10;\';\n' +
-//                     '    scriptTag.onload = function () { this.parentNode.removeChild(this) };\n' +
-//                     '    var container = document.head || document.documentElement;\n' +
-//                     'container.insertBefore(scriptTag, container.children[0]);'
-//             }
-//         );
-//         chrome.tabs.sendMessage(tabs[0].id, {type: 'popup.js'}, function (response) {
-//             console.log(response);
-//         });
-//     });
-// };
-
 // TODO remove - script will send single messages to background
 // connection with background script
 let port = chrome.runtime.connect({name:'ads-popup'});

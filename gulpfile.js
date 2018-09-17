55854968f705ccf4a5f2905140cb31c9d36dbf2e
PawelPodkalicki <ppodkali@gmail.com>
2018-09-17 11:30:53 +0200

    Add development build

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,30 @@ const gulp = require('gulp');
 const webpackStream = require('webpack-stream');
 const webpackConfig = require('./webpack.config.js');
 
-// Bundles js scripts
-gulp.task('bundle-js', () => {
-    return gulp.src('.')
-        .pipe(webpackStream(webpackConfig))
-        .pipe(gulp.dest('./dist/scripts'));
-});
+gulp.task('bundle-js', createBundleJsTask({}));
+
+gulp.task('bundle-js:dev', createBundleJsTask({
+    devMode: true
+}));
+
+/**
+ * Creates task which bundles js scripts.
+ *
+ * @param devMode if true, scripts are minified
+ * @returns {function(): NodeJS.ReadWriteStream}
+ */
+function createBundleJsTask({devMode = false}) {
+    return function () {
+        let config = webpackConfig;
+        if (devMode) {
+            config.devtool = 'cheap-module-source-map';
+            config.mode = 'development';
+        }
+        return gulp.src('.')
+            .pipe(webpackStream(config))
+            .pipe(gulp.dest('./dist/scripts'));
+    };
+}
 
 // Deletes dist folder
 gulp.task('clean', function clean() {
@@ -25,6 +43,15 @@ gulp.task('copy-assets', function copyAssets() {
         '!./src/scripts/*.js',
     ]).pipe(gulp.dest('./dist'));
 });
+
+// Creates distribution resources without minify
+gulp.task('dist:dev',
+    gulp.series(
+        'clean',
+        'copy-assets',
+        'bundle-js:dev'
+    )
+);
 
 // Creates distribution resources
 gulp.task('dist',

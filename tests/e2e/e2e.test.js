6da34825f8aba93cc4544f9139ddbccfc9392016
PawelPodkalicki <ppodkali@gmail.com>
2018-09-20 13:58:47 +0200

    Add e2e test expectation

--- tests/e2e/e2e.test.js
+++ tests/e2e/e2e.test.js
@@ -1,4 +1,4 @@
-const {By} = require('selenium-webdriver');
+const {By, logging} = require('selenium-webdriver');
 const {setupBrowser} = require('./setup');
 
 let driver, extensionId, popupUri;
@@ -18,24 +18,37 @@ describe('positive path test', () => {
     const signature = '7A1CA8AF3246222C2E06D2ADE525A693FD81A2683B8A8788C32B7763DF6037A5' +
         'DF3105B92FEF398AF1CDE0B92F18FE68DEF301E4BF7DB0ABC0AEA6BE24969006';
 
-    async function isPage(pageId) {
+    async function getPendingTxCount() {
+        let txElementArray = await driver.findElements(By.css('#tx-pending > div'));
+        return txElementArray.length;
+    }
+
+    async function isElementDisplayed(pageId) {
         return await driver.findElement(By.id(pageId)).isDisplayed();
     }
 
     async function isPageCreateAccount() {
-        return await isPage('create-acc-page');
+        return await isElementDisplayed('create-acc-page');
     }
 
     async function isPageLogin() {
-        return await isPage('login-page');
+        return await isElementDisplayed('login-page');
     }
 
     async function isPageUser() {
-        return await isPage('user-page');
+        return await isElementDisplayed('user-page');
+    }
+
+    async function isTabTransactions() {
+        return await isElementDisplayed('tab-tx');
+    }
+
+    async function isTabSettings() {
+        return await isElementDisplayed('tab-settings');
     }
 
     test('create account and login', async function () {
-        expect.assertions(2);
+        expect.assertions(3);
 
         // open popup
         await driver.get(popupUri);
@@ -47,14 +60,17 @@ describe('positive path test', () => {
         // create account button
         await driver.findElement(By.id('btn-create-acc')).click();
         expect(await isPageUser()).toBeTruthy();
+        // default tab is tx tab
+        expect(await isTabTransactions()).toBeTruthy();
     });
 
     test('import key', async function () {
-        expect.assertions(1);
+        expect.assertions(6);
 
         // switch to settings tab
         await driver.findElement(By.xpath('//button[@value="tab-settings"]')).click();
         expect(await isPageUser()).toBeTruthy();
+        expect(await isTabSettings()).toBeTruthy();
 
         // input secret key
         await driver.findElement(By.id('imp-key-sk')).sendKeys(secretKey);
@@ -66,14 +82,21 @@ describe('positive path test', () => {
         await driver.findElement(By.id('imp-key-password')).sendKeys(password);
         // import key button
         await driver.findElement(By.id('btn-imp-key')).click();
+
+        // check if form fields are empty - key was imported
+        expect(await driver.findElement(By.id('imp-key-sk')).getAttribute('value')).toBe('');
+        expect(await driver.findElement(By.id('imp-key-pk')).getAttribute('value')).toBe('');
+        expect(await driver.findElement(By.id('imp-key-sg')).getAttribute('value')).toBe('');
+        expect(await driver.findElement(By.id('imp-key-password')).getAttribute('value')).toBe('');
     });
 
     test('log out', async function () {
-        expect.assertions(2);
+        expect.assertions(3);
 
         // switch to settings tab
         await driver.findElement(By.xpath('//button[@value="tab-settings"]')).click();
         expect(await isPageUser()).toBeTruthy();
+        expect(await isTabSettings()).toBeTruthy();
 
         // log out button
         await driver.findElement(By.id('btn-logout')).click();
@@ -81,10 +104,17 @@ describe('positive path test', () => {
     });
 
     test('add transaction', async function () {
+        expect.assertions(1);
+
         // open test page
         await driver.get(`file:///${__dirname}/index.html`);
         // add transaction
         await driver.findElement(By.id('btn-add-tx')).click();
+
+        // check if there are console errors
+        let consoleErrorArray = await driver.manage().logs().get(logging.Type.BROWSER);
+        let errorCount = consoleErrorArray.length;
+        expect(errorCount).toBe(0);
     });
 
     test('log in', async function () {
@@ -102,10 +132,16 @@ describe('positive path test', () => {
     });
 
     test('sign transaction', async function () {
+        expect.assertions(3);
+
         // switch to transaction tab
         await driver.findElement(By.xpath('//button[@value="tab-tx"]')).click();
+        expect(await isTabTransactions()).toBeTruthy();
+        expect(await getPendingTxCount()).toBe(1);
+
         // sign button
         await driver.findElement(By.className('btn-accept')).click();
+        expect(await getPendingTxCount()).toBe(0);
     });
 });
 

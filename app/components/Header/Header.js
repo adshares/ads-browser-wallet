1f24819aec67c8b1cd2ddba147547e24ed768097
Meskat <marciniszyn.k@gmail.com>
2018-12-11 13:39:07 +0100

    Minor menu improvements + select account component

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -4,12 +4,28 @@ import Logo from '../Logo/Logo';
 import Select from '../Select/Select';
 import HamburgerMenu from '../HamburgerMenu/HamburgerMenu';
 
+const TEST_ACCOUNTS = [
+  {
+    name: 'Test 1',
+    account: '0003:000011BB:0001'
+  }, {
+    name: 'Test 2',
+    account: '0013:000011CD:0002'
+  },  {
+    name: 'Test 3',
+    account: '0003:000012BB:0001'
+  },
+];
+
+
 export default class Header extends React.Component {
+
   render() {
+  console.log('accounts', TEST_ACCOUNTS)
     return (
-      <header className={style.header} >
+      <header className={style.header}>
         <Logo className={style.headerLogo} />
-        <Select options={['0003:000011CC:0001', '0003:000011CC:001']} />
+        <Select options={TEST_ACCOUNTS} />
         <HamburgerMenu />
       </header>
     );

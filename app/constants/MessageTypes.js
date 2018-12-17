a0facaf6891547e581dd0027ab9d5183553e1f88
Maciej Pilarczyk <m.pilarczyk@outlook.com>
2018-12-17 23:35:12 +0100

    Comunication

--- app/constants/MessageTypes.js
+++ app/constants/MessageTypes.js
@@ -1,7 +1,5 @@
 export const MSG_PING = 'ping';
 export const MSG_PONG = 'pong';
-export const MSG_INFO_REQUEST = 'infoRequest';
-export const MSG_INFO_RESPONSE = 'infoResponse';
-export const MSG_SIGN_REQUEST = 'signRequest';
-export const MSG_SIGN_RESPONSE = 'signResponse';
+export const MSG_INFO = 'info';
+export const MSG_SIGN = 'sign';
 

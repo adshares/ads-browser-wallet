0b423b8a1e2f006377cb7cd3f3ec42c9877fbbe4
Maciej Pilarczyk <m.pilarczyk@outlook.com>
2019-01-11 16:25:17 +0100

    Refactor transaction page

--- app/containers/Transactions/SendOnePage.js
+++ app/containers/Transactions/SendOnePage.js
@@ -3,8 +3,6 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight, faTimes, faCheck, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 import {
   cleanForm,
   inputChanged,
@@ -13,16 +11,11 @@ import {
   transactionRejected,
 } from '../../actions/transactionActions';
 import TransactionPage from './TransactionPage';
-import Form from '../../components/atoms/Form';
-import Box from '../../components/atoms/Box';
 import InputControl from '../../components/atoms/InputControl';
 import CheckboxControl from '../../components/atoms/CheckboxControl';
-import ButtonLink from '../../components/atoms/ButtonLink';
-import Button from '../../components/atoms/Button';
 import ADS from '../../utils/ads';
 import { fieldLabels } from './labels';
 import style from './TransactionPage.css';
-import config from '../../config/config';
 
 
 class SendOnePage extends TransactionPage {
@@ -41,12 +34,12 @@ class SendOnePage extends TransactionPage {
     super(ADS.TX_TYPES.SEND_ONE, props);
   }
 
-  renderForm() {
+  renderInputs() {
     const {
       inputs: { address, amount, message, rawMessage }
     } = this.props;
     return (
-      <Form onSubmit={this.handleSubmit}>
+      <React.Fragment>
         <InputControl
           name="address"
           label={fieldLabels.recipient}
@@ -88,61 +81,6 @@ class SendOnePage extends TransactionPage {
             />
           </InputControl>
         </div>
-        <div className={style.buttons}>
-          <ButtonLink
-            to={this.getReferrer()}
-            onClick={this.handleCloseForm}
-            inverse
-            icon="left"
-            layout="info"
-            disabled={this.props.isSubmitted}
-          >
-            <FontAwesomeIcon icon={faTimes} /> Cancel
-          </ButtonLink>
-          <Button
-            type="submit"
-            icon="right"
-            layout="info"
-            disabled={this.props.isSubmitted}
-          >Next <FontAwesomeIcon icon={faChevronRight} />
-          </Button>
-        </div>
-      </Form>
-    );
-  }
-
-  renderSuccessInfo() {
-    const { transactionId, transactionFee } = this.props;
-    const addressLink = `${config.operatorUrl}blockexplorer/transactions/`;
-
-    return (
-      <React.Fragment>
-        <Box title="Success" layout="success" icon={faCheck} className={style.transactionSuccess}>
-          Transaction id:
-          <ButtonLink
-            external
-            href={`${addressLink}${transactionId}`}
-            icon="right"
-            layout="contrast"
-            size="wide"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {transactionId}<FontAwesomeIcon icon={faExternalLinkAlt} />
-          </ButtonLink>
-          <small>Transaction fee:
-            <b>{ADS.formatAdsMoney(transactionFee, 11, true)} ADS</b>
-          </small>
-        </Box>
-        <ButtonLink
-          to={this.getReferrer()}
-          onClick={this.handleCloseForm}
-          icon="left"
-          layout="info"
-          size="wide"
-        >
-          <FontAwesomeIcon icon={faTimes} /> Close
-        </ButtonLink>
       </React.Fragment>
     );
   }
--- app/containers/Transactions/TransactionPage.js
+++ app/containers/Transactions/TransactionPage.js
@@ -1,9 +1,22 @@
+/* eslint-disable class-methods-use-this */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { faExclamation } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faExclamation,
+  faCheck,
+  faExternalLinkAlt,
+  faTimes,
+  faChevronRight,
+} from '@fortawesome/free-solid-svg-icons';
+import ADS from '../../utils/ads';
+import config from '../../config/config';
 import PageComponent from '../../components/PageComponent';
 import Page from '../../components/Page/Page';
 import Box from '../../components/atoms/Box';
+import Form from '../../components/atoms/Form';
+import ButtonLink from '../../components/atoms/ButtonLink';
+import Button from '../../components/atoms/Button';
 import SignForm from './SignForm';
 import { typeLabels } from './labels';
 import style from './TransactionPage.css';
@@ -79,6 +92,80 @@ export default class TransactionPage extends PageComponent {
   componentWillUnmount() {
     this.props.actions.cleanForm(
       this.transactionType
+    );
+  }
+
+  renderSuccessInfo() {
+    const { transactionId, transactionFee } = this.props;
+    const addressLink = `${config.operatorUrl}blockexplorer/transactions/`;
+
+    return (
+      <React.Fragment>
+        <Box title="Success" layout="success" icon={faCheck} className={style.transactionSuccess}>
+          Transaction id:
+          <ButtonLink
+            external
+            href={`${addressLink}${transactionId}`}
+            icon="right"
+            layout="contrast"
+            size="wide"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {transactionId}<FontAwesomeIcon icon={faExternalLinkAlt} />
+          </ButtonLink>
+          <small>Transaction fee:
+            <b>{ADS.formatAdsMoney(transactionFee, 11, true)} ADS</b>
+          </small>
+        </Box>
+        <ButtonLink
+          to={this.getReferrer()}
+          onClick={this.handleCloseForm}
+          icon="left"
+          layout="info"
+          size="wide"
+        >
+          <FontAwesomeIcon icon={faTimes} /> Close
+        </ButtonLink>
+      </React.Fragment>
+    );
+  }
+
+  renderInfo() {
+    return '';
+  }
+
+  renderButtons() {
+    return (
+      <div className={style.buttons}>
+        <ButtonLink
+          to={this.getReferrer()}
+          onClick={this.handleCloseForm}
+          inverse
+          icon="left"
+          layout="info"
+          disabled={this.props.isSubmitted}
+        >
+          <FontAwesomeIcon icon={faTimes} /> Cancel
+        </ButtonLink>
+        <Button
+          type="submit"
+          icon="right"
+          layout="info"
+          disabled={this.props.isSubmitted}
+        >Next <FontAwesomeIcon icon={faChevronRight} />
+        </Button>
+      </div>
+    );
+  }
+
+  renderForm() {
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        {this.renderInputs()}
+        {this.renderInfo()}
+        {this.renderButtons()}
+      </Form>
     );
   }
 

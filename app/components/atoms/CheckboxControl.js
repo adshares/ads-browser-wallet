63dc5b1f5089d5f468b796bc5126dc0c6fd29a25
Alex <szychlinska@gmail.com>
2023-01-10 12:44:26 +0100

    custom icons files WIP

--- app/components/atoms/CheckboxControl.js
+++ app/components/atoms/CheckboxControl.js
@@ -13,7 +13,8 @@ const CheckboxControl = ({ name, checked, label, handleChange, readOnly = false 
       <input
         name={name}
         tabIndex="0"
-        className={`${style.checkbox} ${style.checked}`}
+        className={`${style.checkbox}`}
+        // className={`${style.checkbox} ${style.checked}`}
         type="checkbox"
         id={label}
         checked={checked}
@@ -21,11 +22,12 @@ const CheckboxControl = ({ name, checked, label, handleChange, readOnly = false 
         disabled={readOnly}
       />
       {label}
-      <span className={style.checked}>
-        <svg width="18" height="18" viewBox="0 0 17 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M2 4.60188L7.41667 10.2393L15.3333 2" stroke="white" strokeWidth="2" strokeLinecap="square" />
-        </svg>
-      </span>
+      {/*<span className={style.checked}>*/}
+      {/*  <svg width="18" height="18" viewBox="0 0 17 12" fill="none" xmlns="http://www.w3.org/2000/svg">*/}
+      {/*    <path d="M2 4.60188L7.41667 10.2393L15.3333 2"
+      stroke="white" strokeWidth="2" strokeLinecap="square" />*/}
+      {/*  </svg>*/}
+      {/*</span>*/}
     </label>
   );
 };

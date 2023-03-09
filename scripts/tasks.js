30379540bb930dfaa46021bbf3f3eb8c8b9151f9
Alex <szychlinska@gmail.com>
2023-03-09 15:30:50 +0100

    logo animation removed

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -21,16 +21,14 @@ const getContentSecurityPolicy = (isProd) => {
     scriptSrc: ["'self'"],
     connectSrc: ["'self'", 'https://rpc.adshares.net', 'https://rpc.e11.click', 'https://ads-operator.adshares.net', 'https://ads-operator.e11.click', 'data:'],
     styleSrc: ['*', 'blob:', "'unsafe-inline'"],
-    fontSrc: ["'self'", 'https://fonts.gstatic.com'],
+    fontSrc: ["'self'"],
     imgSrc: ["'self'", 'data:'],
-    videoSrc: ["'self'", 'data:'],
   };
 
   if (!isProd) {
     directives.scriptSrc.push('https://localhost:3000', 'http://localhost:3000', "'unsafe-eval'");
     directives.connectSrc.push('https://localhost:3000', 'http://localhost:3000', 'https://127.0.0.1:5000', 'http://127.0.0.1:5000');
     directives.fontSrc.push('https://localhost:3000', 'http://localhost:3000', "'unsafe-eval'");
-    directives.videoSrc.push('https://localhost:3000', 'http://localhost:3000', "'unsafe-eval'");
   }
 
   return { directives };
